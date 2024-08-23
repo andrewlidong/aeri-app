5ea123370b886c23e6730d11af92d69c10ba81e6
Andrew Dong <andrewdong1994@gmail.com>
2024-08-23 08:57:54 -0400

    setup auth, need to setup mongoDB

--- aeri-backend/server.js
+++ aeri-backend/server.js
@@ -1,16 +1,35 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+const authMiddleware = require('./middleware/authMiddleware');
+
+// Load environment variables
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/aeri', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+app.use((req, res, next) => {
+  res.setTimeout(120000, () => { // 2 minutes timeout
+    res.status(503).send('Request timed out');
+  });
+  next();
 });
 
-app.get('/', (req, res) => {
-    res.send('Aeri API');
+// Connect to MongoDB
+connectDB();
+
+// Init Middleware
+app.use(express.json());
+
+// Define Routes
+app.use('/api/auth', require('./routes/authRoutes'));
+
+// Protected route example
+app.get('/protected-route', authMiddleware, (req, res) => {
+  res.json({ msg: 'This is a protected route' });
 });
 
-app.listen(PORT, () => console.log('Server running on port $PORT'));
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
