2904b7e4aa728b7a6d219d1ba7303dc8b3d7c35c
Andrew Dong <andrewdong1994@gmail.com>
2024-08-23 08:38:50 -0400

    create authentication routes

--- aeri-backend/models/User.js
+++ aeri-backend/models/User.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    profilePic: { type: String },
-    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model('User', UserSchema);
